cf73335c23aa395a093d180b0f1c01f8e10bb893
L-M-Sherlock <l.m.kagamigawachloe@gmail.com>
2020-07-19 12:25:39 +0800

    不在需要class.txt

--- models/bert.py
+++ models/bert.py
@@ -1,6 +1,8 @@
 # coding: UTF-8
 import torch
 import torch.nn as nn
+import pandas as pd
+import numpy as np
 # from pytorch_pretrained_bert import BertModel, BertTokenizer
 from pytorch_pretrained import BertModel, BertTokenizer
 
@@ -11,11 +13,11 @@ class Config(object):
     def __init__(self, dataset):
         self.model_name = 'bert'
         self.train_path = dataset + '/data/train.txt'  # 训练集
-        self.dev_path = dataset + '/data/dev.txt'  # 验证集
-        self.test_path = dataset + '/data/test.txt'  # 测试集
         self.log_path = dataset + '/log/' + self.model_name
-        self.class_list = [x.strip() for x in open(
-            dataset + '/data/class.txt', encoding='utf-8').readlines()]  # 类别名单
+        data = pd.read_csv(self.train_path, encoding='utf-8', names=['comments', 'label'], sep='\t__label__', header=None, engine='python')
+        label = np.array(data['label'])
+        class_set = set(label.flatten().tolist())
+        self.class_list = list(class_set)
         self.save_path = dataset + '/saved_dict/' + self.model_name + '.ckpt'  # 模型训练结果
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # 设备
 
